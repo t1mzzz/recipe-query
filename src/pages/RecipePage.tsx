54f9b90a3097a2de180a561e412583196169fc6a
t1mzzz <timothykoei@gmail.com>
2023-10-21 19:19:06 +0800

    Change title

--- src/pages/RecipePage.tsx
+++ src/pages/RecipePage.tsx
@@ -13,7 +13,7 @@ export default function RecipePage(): ReactElement {
 			? <div className="min-h-screen min-w-screen bg-[#282c34] flex flex-col px-20 py-40 space-y-4">
 					<div className="grid place-items-center">
 						<h2 className="text-3xl font-medium text-white">
-							Recipe Query
+							Random Recipe
 						</h2>
 					</div>
 					<div className="grid place-items-center">
